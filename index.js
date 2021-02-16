ccbe2fcb919b7ea3e195888e4646fabb69e22398
Justin Rovnak <jmrovnak@gmail.com>
2021-02-16 13:50:44 -0600

    added first 4 questions title, description, usage, installation to index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+function userPrompt () {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "projectTitle",
+            message: "What is the title of your project?"
+        },
+
+        {
+            type: "input",
+            name: "description",
+            message: "Please enter a brief description of your project."
+        },
+
+        {
+            type: "input",
+            name: "installation",
+            message: "Please enter installation instructions for your project."
+        },
+        
+        {
+            type: "input",
+            name: "usage",
+            message: "What is the intended usage of this application?"
+        }
+
+    ])
+}
